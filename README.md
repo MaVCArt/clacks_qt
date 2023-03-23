9f9de276c78c1e5e36df677b6a68b0cd32050b90
MaVCArt <mavcart.mvc@gmail.com>
2023-03-23 17:04:36 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# clacks_qt
- Extension library for the Clacks framework that provides qt-specific utilities like a thread-safe, qt event-driven server class.
+clacks_qt
+=========
+
+Extension library for the Clacks framework that provides qt-specific utilities like a thread-safe, qt event-driven
+server class.
+
+Qt Server
+---------
+
+The Qt Server implementation makes use of a clever trick with `Qt` Signals, to create a server that will work inside
+applications that are otherwise not friendly to threads. A typical example of this would be Unreal Engine, Maya or 3ds
+Max, which can technically run threads in their python interpreters, but start to act up if that thread attempts to
+anything to the main thread. This is also common behaviour in Qt Applications.
+
+Signals solve this problem, as `QThreads` and thread workers don't actually run in a separate thread; they run in the
+main thread, but Qt ensures they don't block it.
+
+This type of structure allows us to run the logistics of the server (I/O, digest, marshalling) on a real thread,
+while the non-thread-safe behaviour is handled on the main thread itself.
---
+++ server.py
@@ -0,0 +1,34 @@
+from vendor import Qt
+from clacks.core.server import ServerBase
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+class QtServerBase(Qt.QtCore.QObject, ServerBase):
+
+    RespondRequested = Qt.QtCore.Signal(object, object, object, object, object)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def __init__(self, identifier, **kwargs):
+        Qt.QtCore.QObject.__init__(self)
+
+        ServerBase.__init__(
+            self,
+            identifier=identifier,
+            start_queue=False,
+            threaded_digest=False,
+        )
+
+        # -- Qt redirect to prevent methods from being executed off the main thread.
+        self.RespondRequested.connect(self.__respond)
+
+        self.start_queue()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def __respond(self, handler, connection, transaction_id, header_data, data):
+        # type: (BaseRequestHandler, socket.socket, str, dict, dict) -> None
+        ServerBase._respond(self, handler, connection, transaction_id, header_data, data)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def _respond(self, handler, connection, transaction_id, header_data, data):
+        # type: (BaseRequestHandler, socket.socket, str, dict, dict) -> None
+        self.RespondRequested.emit(handler, connection, transaction_id, header_data, data)
